260e9b57d266d2a3870f4e07453891d986c20933
ASP.NET <ngochang18032005@gmail.com>
2026-03-20 10:39:03 +0700

    add full source code

---
+++ VANTHINGOCHANG-2123110352/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VANTHINGOCHANG_2123110352.Models;
+
+namespace VANTHINGOCHANG_2123110352.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Constructor nhận options từ Program.cs
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
